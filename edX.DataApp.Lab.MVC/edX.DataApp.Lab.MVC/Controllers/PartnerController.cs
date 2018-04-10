307ee7bba8ab48bec273916d6c609ea508755945
Serj <serj.dukareff@gmail.com>
2018-04-10 23:06:52 +0300

    Module 3 completed

---
+++ edX.DataApp.Lab.MVC/edX.DataApp.Lab.MVC/Controllers/PartnerController.cs
@@ -0,0 +1,59 @@
+﻿using edX.DataApp.Lab.MVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace edX.DataApp.Lab.MVC.Controllers
+{
+    public class PartnerController : Controller
+    {
+        // GET: Partners
+        public ActionResult Index()
+        {
+            List<Partner> model = new List<Partner>();
+            using (ContosoContext context = new ContosoContext())
+            {
+                model.AddRange(context.Partners);
+                return View(model);
+            }
+        }
+
+        // GET: Partner Details
+        public ActionResult Details(int id)
+        {
+            Partner model;
+            using (ContosoContext context = new ContosoContext())
+            {
+                model = context.Partners.Find(id);
+                return View(model);
+            }
+        }
+
+        // GET: Question 1 Answer
+        public ActionResult Question1()
+        {
+            List<Partner> model = new List<Partner>();
+            using (ContosoContext context = new ContosoContext())
+            {
+                model.AddRange(context.Partners.Where(p => !p.IsOpen)
+                    .OrderBy(p => p.Name));
+                return View(model);
+            }
+        }
+
+        // GET: Question 3 Answer
+        public ActionResult Question3()
+        {
+            string name = "Comtour";
+            Partner model;
+            using (ContosoContext context = new ContosoContext())
+            {
+                model = context.Partners
+                    .FirstOrDefault(p => String.Equals(p.Name, name));
+                return View(model);
+            }
+        }
+    }
+}
---
+++ edX.DataApp.Lab.MVC/edX.DataApp.Lab.MVC/Models/ContosoContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace edX.DataApp.Lab.MVC.Models
+{
+    public class ContosoContext : DbContext
+    {
+        public ContosoContext() : base(@"Data Source=(localdb)\MSSQLLOCALDB;Initial Catalog=ContosoDB;")
+        { }
+
+        public DbSet<Partner> Partners { get; set; }
+    }
+}
